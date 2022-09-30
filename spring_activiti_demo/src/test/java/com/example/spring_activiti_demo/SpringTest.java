327851ba7ccbf408c986f7e542ec2f6ae10123ef
b286951543 <892153580@qq.com>
2022-09-30 14:59:20 +0800

    add

---
+++ spring_activiti_demo/src/test/java/com/example/spring_activiti_demo/SpringTest.java
@@ -0,0 +1,22 @@
+package com.example.spring_activiti_demo;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RepositoryService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:activiti-spring.xml")
+public class SpringTest {
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @Test
+    public void testGet() {
+        System.out.println(repositoryService);
+    }
+}
